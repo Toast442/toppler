cf07c69f3a76c50e5a6e6be8b09f81c45486ffe9
Jason Kingan <jasonk@toast442.org>
2021-11-08 15:40:48 -0700

    Updated the build slightly to add codesigning
    Now properly signs the frameworks recursively.

--- macfiles/make-bundle.sh
+++ macfiles/make-bundle.sh
@@ -12,7 +12,9 @@ mkdir -p ${BUNDLE}/Contents/Frameworks
 cat macfiles/Info.plist | sed -e "s/@VERSION@/${1}/" > ${BUNDLE}/Contents/Info.plist
 cp macfiles/PkgInfo ${BUNDLE}/Contents
 cp macfiles/Toppler.icns ${BUNDLE}/Contents/Resources
-cp -a $SDL/* ${BUNDLE}/Contents/Frameworks
+cp -a $SDL/SDL2.framework ${BUNDLE}/Contents/Frameworks
+cp -a $SDL/SDL2_image.framework ${BUNDLE}/Contents/Frameworks
+cp -a $SDL/SDL2_mixer.framework ${BUNDLE}/Contents/Frameworks
 cp -a toppler ${BUNDLE}/Contents/MacOS
 cp -a *.ttm ${BUNDLE}/Contents/Resources
 cp -a *.ogg ${BUNDLE}/Contents/Resources
@@ -20,7 +22,6 @@ cp -a toppler.dat ${BUNDLE}/Contents/Resources
 
 
 rm -vf ${BUNDLE}/Contents/Frameworks/SDL2.framework/Versions/Current/Headers/SDL2
-
 
 MACOS_APP_BIN=${BUNDLE}/Contents/MacOS/Toppler
 
@@ -30,4 +31,11 @@ for old in `otool -L $MACOS_APP_BIN | grep @rpath | cut -f2 | cut -d' ' -f1`; do
     install_name_tool -change $old $new $MACOS_APP_BIN
 done
 
+pushd ${BUNDLE}/Contents/Frameworks > /dev/null 2>&1
+signframework *
+popd > /dev/null 2>&1
+sign --options=runtime --entitlements=macfiles/toppler-Entitlements.plist ${BUNDLE}
 
+VERSION=$(/usr/libexec/PlistBuddy  -c "Print CFBundleGetInfoString" ${BUNDLE}/Contents/Info.plist)
+
+ditto -c -k --keepParent ${BUNDLE} Toppler-$VERSION.zip
